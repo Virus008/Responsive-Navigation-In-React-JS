c31788d05c237cf4c66910d4f0fbab298ac5a839
Virus008 <sonavalevrushabh@gmail.com>
2020-02-21 14:02:57 +0530

    About & Contact Pages Added

---
+++ my-navigation/src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+
+class About extends Component {
+    render() {
+        return(
+            <div className="container jumbotron">
+                <h3>Hello,</h3>
+                <p>This is About page.</p>
+            </div>
+        )
+    }
+}
+
+
+export default About;
---
+++ my-navigation/src/components/pages/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+
+class Contact extends Component {
+    render() {
+        return(
+            <div className="container jumbotron">
+                <h3>Hello,</h3>
+                <p>This is Contact page.</p>
+            </div>
+        )
+    }
+}
+
+
+export default Contact;
