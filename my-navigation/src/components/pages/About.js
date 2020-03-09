d184241b5c9ed034d228f763485440524a435d52
Virus008 <sonavalevrushabh@gmail.com>
2020-03-09 10:36:54 +0530

    New pages added.

--- my-navigation/src/components/pages/About.js
+++ my-navigation/src/components/pages/About.js
@@ -4,9 +4,11 @@ import ReactDom from 'react-dom';
 class About extends Component {
     render() {
         return(
-            <div className="container jumbotron">
-                <h3>Hello,</h3>
-                <p>This is About page.</p>
+            <div className="container">
+                <div className="jumbotron">
+                    <h3>Hello,</h3>
+                    <p>This is About page.</p>
+                </div>                
             </div>
         )
     }
--- my-navigation/src/components/pages/Contact.js
+++ my-navigation/src/components/pages/Contact.js
@@ -4,10 +4,13 @@ import ReactDom from 'react-dom';
 class Contact extends Component {
     render() {
         return(
-            <div className="container jumbotron">
-                <h3>Hello,</h3>
-                <p>This is Contact page.</p>
+            <div className="container">
+                <div className="jumbotron">
+                    <h3>Hello,</h3>
+                    <p>This is Contact page.</p>
+                </div>    
             </div>
+            
         )
     }
 }
