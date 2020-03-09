7d9828abe8d9ad37adb9b171106943e48f9f3a07
Virus008 <sonavalevrushabh@gmail.com>
2020-03-09 10:34:14 +0530

    Components added to render.

--- my-navigation/src/index.js
+++ my-navigation/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Header from './components/Header';
+// import App from './App';
+// import Navigation1 from './components/Navigation1';
+import Navigation2 from './components/Navigation2';
+// import Navigation3 from './components/Navigation3';
 
-ReactDOM.render(<Header />, document.getElementById('root'));
+
+// ReactDOM.render(<Header />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<Navigation1 />, document.getElementById('root'));
+ReactDOM.render(<Navigation2 />, document.getElementById('root'));
+// ReactDOM.render(<Navigation3 />, document.getElementById('root'));
 
 serviceWorker.unregister();
+
+
+
