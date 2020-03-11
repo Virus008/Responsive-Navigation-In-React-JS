d62f07336a9c40d503c58fc302360648c0c1f86a
Virus008 <sonavalevrushabh@gmail.com>
2020-03-11 09:57:09 +0530

    Title changed to Virus008

--- my-navigation/src/components/Header.js
+++ my-navigation/src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
         <div>
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container">
-                    <a class="navbar-brand" href="#">Navbar</a>
+                    <a class="navbar-brand" href="#">Virus008</a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mynav" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
