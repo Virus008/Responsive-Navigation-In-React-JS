0619fa00e5eff84b248b43d1f627510763736aae
Virus008 <sonavalevrushabh@gmail.com>
2020-03-12 10:37:25 +0530

    Browser routing added

--- my-navigation/src/components/Navigation1.js
+++ my-navigation/src/components/Navigation1.js
@@ -1,7 +1,40 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import {Link, MemoryRouter as Router, Route} from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home1 from './pages/Home1';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 
-//this is  navigation using memory router
+//this is  navigation using (browser) router  i.e. page relod
+
+class Navigation1 extends React.Component {
+    render() {
+        return(
+            <div>
+                <Router>
+                    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                        <div className="container">
+                            <a href="/" className="navbar-brand">Virus008</a>
+                            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mynav1" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                            <div className="collapse navbar-collapse" id="mynav1">
+                                <div className="navbar-nav">
+                                    <a href="/about" className="nav-item nav-link">About</a>
+                                    <a href="/contact" className="nav-item nav-link">Contact</a>
+                                </div>
+                            </div>
+                        </div>
+                    </nav>
+                    <br></br>
+                    <div className="content">
+                        <Route exact path="/" component={Home1}></Route>
+                        <Route exact path="/about" component={About}></Route>
+                        <Route exact path="/contact" component={Contact}></Route>
+                    </div>
+                </Router>
+            </div>
+        )
+    }
+}
 export default Navigation1;
